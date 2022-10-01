b62e6d5b17a6ec5f1ad70badabebdbf83cf91507
Alona Stupak <iakhdesigner@gmail.com>
2022-10-01 15:20:11 +0300

    11.3

--- 11/03/index.php
+++ 11/03/index.php
@@ -2,13 +2,24 @@
 function capitalize( $cityName ): string
 {
     return mb_convert_case($cityName, MB_CASE_TITLE, "UTF-8");
-    }
+}
 var_dump( capitalize( 'нью-йорк' ) );
 var_dump( capitalize( 'киЇв' ) );
 
-/*function capitalizeSeparator( $newCityName ): string
+function capitalizeSeparator( $newCityName ): string
 {
-    $separator = [ 'на', 'де' ];
-    return ... //І тут мені потрібна допомога, бо не розумію, як цей розділювач зробити(((
+    $newCityName = mb_strtolower($newCityName);
+    $newCityNameArray = explode('-', $newCityName);
+    $newCityNameMap = array_map(
+        function ($text)
+        {
+            $separator = [ 'на', 'де' ];
+            return in_array($text, $separator)
+            ? mb_strtolower($text)
+            : capitalize($text);
+        },
+        $newCityNameArray
+    );
+    return implode('-', $newCityNameMap);
 }
 echo capitalizeSeparator('ФраНкфурт-На-майні');
